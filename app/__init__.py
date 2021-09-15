da6477f630f5cf1a99f33cd816934c159ea7e9e4
Nathan Harmon <22229517+nathanharmon1@users.noreply.github.com>
2021-09-14 20:11:20 -0500

    make the website load an index page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 DB_NAME = "DB_NAME"
 
@@ -29,5 +29,9 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
     return app
